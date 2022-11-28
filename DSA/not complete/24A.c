2dfaa8976e58dd1cf2cc8abac11a367220e6adb9
Dvs <business.divyashah@gmail.com>
2022-11-28 11:20:59 +0530

    remaining slips

--- DSA/not complete/24A.c
+++ DSA/not complete/24A.c
@@ -1,1 +1,5 @@
-/*https://www.thecrazyprogrammer.com/2014/02/c-program-and-algorithm-for-evaluation-of-a-postfix-expression.html*/
+/*https://www.thecrazyprogrammer.com/2014/02/c-program-and-algorithm-for-evaluation-of-a-postfix-expression.html
+
+Write a 'C' program to read a postfix expression, evaluate it and display the result.
+	 (Use Dynamic Implementation of Stack)
+*/
--- DSA/not complete/28B.c
+++ DSA/not complete/28B.c
@@ -1,2 +1,2 @@
-/*Write a 'C' program to read n integers and create two lists such that all positive numbers are in one list and negative numbers are in another list. 
-Display both the lists.*/
+/*Write a 'C' program to read n integers and create two lists such that all positive numbers are in one list and negative numbers 
+are in another list. Display both the lists.*/
